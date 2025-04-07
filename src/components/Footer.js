01d89088af3af5e8f1a16f353921021245c2b713
Manohar Ravula <99091645+manoharyadavr@users.noreply.github.com>
2025-04-07 17:53:12 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -92,7 +92,13 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faInstagram, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebook,
+  faInstagram,
+  faYoutube,
+  faTwitter,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   // Track screen dimensions
