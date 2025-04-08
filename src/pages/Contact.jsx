8e86fdbae1c8dfbe2e77e9b1cbc697a5f577c062
Manohar Ravula <99091645+manoharyadavr@users.noreply.github.com>
2025-04-08 18:27:44 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -205,7 +205,7 @@ const Contact = () => {
           <iframe
             title="Google Maps"
             className="w-full h-full rounded-lg"
-            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3804.158455111106!2d78.23671307493863!3d17.547640983367113!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTfCsDMyJzUxLjUiTiA3OMKwMTQnMjEuNCJF!5e0!3m2!1sen!2sin!4v1743771587931!5m2!1sen!2sin"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3804.1800328137247!2d78.23676139999999!3d17.546613199999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcbf39f65376b3f%3A0x4ce4a0ac3455c37c!2sBYV!5e0!3m2!1sen!2sin!4v1744116845826!5m2!1sen!2sin"
             allowFullScreen
             loading="lazy"
           ></iframe>
